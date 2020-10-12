a1ae76a50bbb6fe4883dbbfba999c4c683ac6951
arpitbhardwaj <arpitbhardwaj09@gmail.com>
2020-10-12 11:46:28 +0530

    [Added Sample Get Request]

---
+++ src/com/ab/servlets/GetServlet.java
@@ -0,0 +1,25 @@
+package com.ab.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+//@WebServlet("/getServlet")
+public class GetServlet extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String name = req.getParameter("name");
+		String htmlResponse = "<html><h3>Welcome to Servlets!!</h3></html>";
+		PrintWriter writer = resp.getWriter();
+		writer.write(htmlResponse + " " + name);
+	}
+	
+	
+}
